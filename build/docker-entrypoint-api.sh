4cf6410131439082af296ab5dc80cf156591ca30
Lucas Semprebom <lucass.semprebom@gmail.com>
2020-02-28 16:56:28 -0300

    feat: generate token keys in the build

--- build/docker-entrypoint-api.sh
+++ build/docker-entrypoint-api.sh
@@ -4,4 +4,8 @@ cd /api/
 
 python manage.py migrate
 python manage.py generate_geodb
+
+ssh-keygen -t rsa -b 4096 -m PEM -f /api/snow/api/snow/settings/jwtRS256.key -P ""
+openssl rsa -in jwtRS256.key -pubout -outform PEM -out /api/snow/api/snow/settings/jwtRS256.key.pub
+
 exec "$@"
